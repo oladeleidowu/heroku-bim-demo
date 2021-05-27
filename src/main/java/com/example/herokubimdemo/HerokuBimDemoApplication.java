042dfb8624f2e334a91cb822e99954532c223198
Oladele Idowu <olataurus15@gmail.com>
2021-05-27 13:55:58 +0100

    initial commit

---
+++ src/main/java/com/example/herokubimdemo/HerokuBimDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.herokubimdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerokuBimDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerokuBimDemoApplication.class, args);
+	}
+
+}
