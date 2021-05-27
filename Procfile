54da1360843950d25b238bcb117ac232fcb9a27f
Oladele Idowu <olataurus15@gmail.com>
2021-05-27 19:41:12 +0100

    new port

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
-web: java $JAVA_OPTS -jar target/heroku-demo-0.0.1-SNAPSHOT.jar -Dserver.port=$PORT $JAR_OPTS
+ web java $JAVA_OPTS -jar target/dependency/webapp-runner.jar --port $PORT target/*.war
+ JAVA_TOOL_OPTIONS -Xmx300m -Xss512k -XX:CICompilerCount=2 -Dfile.encoding=UTF-8
--- src/main/java/com/example/herokubimdemo/HerokuBimDemoApplication.java
+++ src/main/java/com/example/herokubimdemo/HerokuBimDemoApplication.java
@@ -2,11 +2,13 @@ package com.example.herokubimdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
 @ResponseBody
+@EnableConfigurationProperties
 public class HerokuBimDemoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
