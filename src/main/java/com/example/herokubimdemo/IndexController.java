bb59e2673d633946318ab154263ac1e6f08a208c
Oladele Idowu <olataurus15@gmail.com>
2021-05-27 19:48:10 +0100

    again

--- src/main/java/com/example/herokubimdemo/IndexController.java
+++ src/main/java/com/example/herokubimdemo/IndexController.java
@@ -1,11 +1,11 @@
 package com.example.herokubimdemo;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class IndexController {
-    @RequestMapping("/")
+    @GetMapping("/")
     public String index() {
         return "Hello there! I'm running.";
     }
