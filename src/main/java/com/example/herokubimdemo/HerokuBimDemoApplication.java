fd915f1c40e2d1f3ee11f4fcf060c91948bac993
Oladele Idowu <olataurus15@gmail.com>
2021-05-27 17:24:32 +0100

    Adding Dynos

--- src/main/java/com/example/herokubimdemo/HerokuBimDemoApplication.java
+++ src/main/java/com/example/herokubimdemo/HerokuBimDemoApplication.java
@@ -2,9 +2,12 @@ package com.example.herokubimdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
-public class HerokuBimDemoApplication {
+@ResponseBody
+public class HerokuBimDemoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HerokuBimDemoApplication.class, args);
