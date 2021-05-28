ac259939e2c85965b16faef88f9e83057ff0495d
Oladele Idowu <olataurus15@gmail.com>
2021-05-28 07:52:35 +0100

    We commit

--- src/main/java/com/example/herokubimdemo/HerokuBimDemoApplication.java
+++ src/main/java/com/example/herokubimdemo/HerokuBimDemoApplication.java
@@ -2,15 +2,19 @@ package com.example.herokubimdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication
-@ResponseBody
 @Controller
+@SpringBootApplication
 public class HerokuBimDemoApplication extends SpringBootServletInitializer {
 
+	@RequestMapping("/")
+	@ResponseBody
+	String index() {
+		return "Hello there! I'm running.OK!";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(HerokuBimDemoApplication.class, args);
 	}
